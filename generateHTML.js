6627539ec887f2168cf4137c32247550292226ac
Matt <matthiasvstephens@gmail.com>
2021-10-18 16:07:13 +0200

    Add target blank to a tags

--- generateHTML.js
+++ generateHTML.js
@@ -1,45 +1,24 @@
 const fs = require('fs');
 
-// const generateCards = (objArray)=>new Promise((resolve, reject)=>{
-//   if (objArray){
-//     let cardsArr = [];
-//     let engOrInt = objArray[0].school ? 'Intern' : 'Engineer'
-//     for (let obj of objArray){
-//       let schoolOrGit = obj.school ? `<p>School: ${obj.school}</p>` : `<p><a href="https://github.com/${obj.github}">GitHub</a></p>`
-//       cardsArr.push(`<div class="card">
-//       <h2>${engOrInt}</h2>
-//       <h1>${obj.name}</h1>
-//       <h3>Employee ID: ${obj.id}</h3>
-//       <p><a href="mailto:${obj.email}">Email</a></p>
-//       ${schoolOrGit}
-//     </div>`)
-//     }
-//     resolve(cardsArr);
-//   } else {
-//     reject(err);
-//   }
-// })
-
 const generateCards = (objArray)=>new Promise((resolve, reject)=>{
     if (objArray.length !== 0){
       let cardsArr = [];
       let engOrInt = objArray[0].school ? 'Intern' : 'Engineer'
       for (let obj of objArray){
-        let schoolOrGit = obj.school ? `<p>School: ${obj.school}</p>` : `<p><a href="https://github.com/${obj.github}">GitHub</a></p>`
+        let schoolOrGit = obj.school ? `<p>School: ${obj.school}</p>` : `<p><a href="https://github.com/${obj.github} target="_blank">GitHub</a></p>`
         cardsArr.push(`<div class="card">
         <h2>${engOrInt}</h2>
         <h1>${obj.name}</h1>
         <h3>Employee ID: ${obj.id}</h3>
-        <p><a href="mailto:${obj.email}">Email</a></p>
+        <p><a href="mailto:${obj.email} target="_blank">Email</a></p>
         ${schoolOrGit}
-      </div>`)
+    </div>`)
       }
       resolve(cardsArr);
     } else {
       reject(new Error(err));
     }
   })
-
 
 function createDocument(man, cards){
   let containers;
@@ -71,7 +50,7 @@ function createDocument(man, cards){
       <h2>Manager</h2>
       <h1>${man.managerName}</h1>
       <h3>Employee ID: ${man.managerId}</h3>
-      <p><a href="mailto:${man.managerEmail}">Email</a></p>
+      <p><a href="mailto:${man.managerEmail}" target="_blank>Email</a></p>
       <p>Office #: ${man.managerOffice}</p>
     </div>
   </div>
@@ -80,14 +59,6 @@ function createDocument(man, cards){
   </html>
   `
 }
-
-// function generateHTML(data){
-//   const parsed = JSON.parse(data);
-//   const manager = parsed.manager;
-//   let promises = [generateCards(parsed.engineers), generateCards(parsed.interns)]
-//   Promise.all(promises)
-//     .then(result => fs.writeFile('./dist/myTeam.html', createDocument(manager, result), err => err ? console.error(err): null));
-// }
 
 function generateHTML(data){
   const parsed = JSON.parse(data);
@@ -107,9 +78,9 @@ function generateHTML(data){
 
 module.exports = generateHTML;
 
+// pseudocode for self
+
 // in generateHTML, create a function which for each engineer generates the code for a card with their information and pushes it into an array
 // in generateHTML, do the same function for interns
 // Promise.all those two functions, then once resolved, take the end results and use them accordingly in main generateHTML function
 // return template literal
-
-// MAYBE you can just put functions inside of template literals? then it will be fairly easier
